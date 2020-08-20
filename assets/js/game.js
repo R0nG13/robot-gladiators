80c043feeb5b4a220fe8b9e6c8a2bce9ddcee68a
Ronald Gould <ronaldgould@ronalds-air.home>
2020-08-20 18:40:16 -0400

    start and end functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -72,6 +72,11 @@ var fight = function(enemyName) {
   }
 };
 
+var startGame = function() {
+  
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
 
 for (var i = 0; i < enemyNames.length; i++) {
   if (playerHealth > 0) {
@@ -79,7 +84,7 @@ for (var i = 0; i < enemyNames.length; i++) {
     window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
 
     // pick new enemy to fight based on the index of the enemyNames array
-    var pickedEnemyName = enemyNames[i++];
+    var pickedEnemyName = enemyNames[i];
 
     // reset enemyHealth before starting new fight
     enemyHealth = 50;
@@ -89,12 +94,43 @@ for (var i = 0; i < enemyNames.length; i++) {
 
     // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
     fight(pickedEnemyName);
+    break;
   }
   else {
     window.alert("You have lost your robot in battle! Game Over!");
     break;
+    }
   }
-  var pickedEnemyName = enemyNames[i];
-  enemyHealth = 50;
-  fight(pickedEnemyName);
+
+  
+  //var pickedEnemyName = enemyNames[i++];
+  //enemyHealth = 50;
+  //fight(pickedEnemyName);
+// after the loop ends, player is either out of health or enemies to fight, so run the endGame function
+endGame();
+};
+
+// function to end the entire game
+var endGame = function() {
+  // if player is still alive, player wins!
+  if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+  } 
+  else {
+    window.alert("You've lost your robot in battle.");
+  }
+  // ask player if they'd like to play again
+var playAgainConfirm = window.confirm("Would you like to play again?");
+
+if (playAgainConfirm) {
+  // restart the game
+  startGame();
+} 
+else {
+  window.alert("Thank you for playing Robot Gladiators! Come back soon!");
 }
+};
+
+
+// start the game when the page loads
+startGame();
